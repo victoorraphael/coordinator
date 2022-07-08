b687cc620d317593e4e4749411f024d5892d6d19
Raphael <raphaelvictor27@gmail.com>
2022-07-08 17:17:48 -0300

    feat: :zap: add route ping-pong and simple Makefile

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,1 +1,16 @@
-package http
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "pong!")
+	})
+
+	e.Logger.Fatal(e.Start(":3030"))
+}
