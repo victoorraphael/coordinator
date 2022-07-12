3bb02dfaa62e10561e2da0a337a625a734675357
Raphael <raphaelvictor27@gmail.com>
2022-07-12 12:08:12 -0300

    build: PORT as env variable

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
+	"os"
 )
 
 func main() {
+	PORT := os.Getenv("PORT")
+
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	e.GET("/ping", func(c echo.Context) error {
 		return c.String(http.StatusOK, "pong!")
 	})
 
-	e.Logger.Fatal(e.Start(":3030"))
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", PORT)))
 }
