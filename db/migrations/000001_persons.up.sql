3d61c4304dc23eb21c6604f0f1668d01086e628f
Raphael <raphaelvictor27@gmail.com>
2022-11-17 21:35:06 -0300

    refactor(db): adjust person table to use id serial in relations

--- db/migrations/000001_persons.up.sql
+++ db/migrations/000001_persons.up.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS persons(
-  id uuid DEFAULT uuid_generate_v4 (),
+  id SERIAL,
+  uuid uuid DEFAULT uuid_generate_v4(),
   name varchar(255),
   email varchar(255),
   phone varchar(11),
   birthdate timestamp,
-  created_at timestamp,
+  created_at timestamp default now(),
 
   PRIMARY KEY (id)
 );
